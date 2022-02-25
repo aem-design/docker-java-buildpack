23549e6e262f0a480c8f4fe4ff999207e9c7ce8f
wildone <max.barrass@gmail.com>
2022-02-25 22:43:10 +1100

    add docker analysis.

--- build.ps1
+++ build.ps1
@@ -5,7 +5,8 @@ Param(
   [string]$TAG = "jdk8-ubuntu",
   [string]$FILE = "Dockerfile",
   [string]$FUNCTIONS_URI = "https://github.com/aem-design/aemdesign-docker/releases/latest/download/functions.ps1",
-  [string]$COMMAND = "docker buildx build . -f .\${FILE} -t ${TAG}"
+  [string]$COMMAND = "docker buildx build . -f .\${FILE} -t ${TAG}",
+  [string]$TEST = "docker run --rm -it -e CI=true -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive:latest ${TAG}"
 )
 
 $SKIP_CONFIG = $true
@@ -17,5 +18,6 @@ printSectionBanner "Building Image"
 printSectionLine "$COMMAND" "warn"
 
 Invoke-Expression -Command "$COMMAND" | Tee-Object -Append -FilePath "${LOG_FILE}"
+Invoke-Expression -Command "$TEST" | Tee-Object -Append -FilePath "${LOG_FILE}"
 
 
